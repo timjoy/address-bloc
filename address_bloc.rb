5b901dcf2b05ae726912d89ac313790ab547d734
Timothy Joy <tim.joy47@gmail.com>
2017-12-09 09:18:11 -0600

    added services to create an entry, view all entries, and display a submenu

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,10 @@
-puts "Welcome to AddressBloc!"
+# puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+
+ menu = MenuController.new
+
+ system "clear"
+ puts "Welcome to AddressBloc!"
+
+ menu.main_menu
